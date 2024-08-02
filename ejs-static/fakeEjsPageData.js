3fe71a4b86f2388c1ec0bb02c9b3ac6d2d42fbfe
Julio <Julio.Navarro.Coder@gmail.com>
2024-08-02 15:13:47 -0700

    node server for quick view of ejs pages

---
+++ ejs-static/fakeEjsPageData.js
@@ -0,0 +1,168 @@
+const ownedRoomInfo = {
+  roomName: "fakeRoomName",
+  RoomID: "fakeRoomID1",
+  createdAt: "2024-08-02T21:34:22.328Z",
+};
+
+const joinedRoomInfo = {
+  roomName: "fakeJoinRoomName",
+  RoomID: "fakeRoomID2",
+  createdAt: "2024-08-02T21:34:22.328Z",
+};
+
+const fakeUserInfo = {
+  userID: "fd24d28c-ef01-45dc-a1ee-2bf8f3e92540",
+  userName: "TestUserRender",
+  email: "TestUserRender@test.com",
+  ownedRooms: [
+    {
+      roomName: ownedRoomInfo.roomName,
+      RoomID: ownedRoomInfo.RoomID,
+    },
+  ],
+  joinedRooms: [
+    {
+      roomName: joinedRoomInfo.roomName,
+      RoomID: joinedRoomInfo.RoomID,
+      isAdmin: false,
+    },
+  ],
+  profileColor: "green",
+};
+
+const fakeDashboardData = {
+  ownedRooms: fakeUserInfo.ownedRooms,
+  joinedRooms: fakeUserInfo.joinedRooms,
+  username: fakeUserInfo.userName,
+  profileColor: fakeUserInfo.profileColor,
+  navJoinRequest: [],
+  isProduction: false,
+};
+
+const fakeJoinRoomInfoPage = {
+  roomInfo: joinedRoomInfo,
+  roomOwner: {
+    userID: "FakeOwnerID",
+    userName: "FakeRoomOwner",
+    RoomID: joinedRoomInfo.RoomID,
+    RoomUserStatus: "OWNER",
+    profileColor: "orange",
+    joinedAt: "2024-08-02T21:34:22.328Z",
+  },
+  roomMembers: [
+    {
+      userID: "FakeOwnerID",
+      userName: "FakeRoomOwner",
+      RoomID: joinedRoomInfo.RoomID,
+      RoomUserStatus: "OWNER",
+      profileColor: "orange",
+      joinedAt: "2024-08-02T21:34:22.328Z",
+    },
+    {
+      userID: fakeUserInfo.userID,
+      userName: fakeUserInfo.userName,
+      RoomID: joinedRoomInfo.RoomID,
+      RoomUserStatus: "MEMBER",
+      profileColor: fakeUserInfo.profileColor,
+      joinedAt: "2024-08-02T21:34:22.328Z",
+    },
+  ],
+  isMember: false,
+  isOwnerOrAdmin: false,
+  isOwner: false,
+  navJoinRequest: [],
+  profileColor: fakeUserInfo.profileColor,
+  username: fakeUserInfo.userName,
+  isProduction: false,
+};
+
+const fakeJoinRoomInfoPageMember = {
+  roomInfo: joinedRoomInfo,
+  roomOwner: {
+    userID: "FakeOwnerID",
+    userName: "FakeRoomOwner",
+    RoomID: joinedRoomInfo.RoomID,
+    RoomUserStatus: "OWNER",
+    profileColor: "orange",
+    joinedAt: "2024-08-02T21:34:22.328Z",
+  },
+  roomMembers: [
+    {
+      userID: "FakeOwnerID",
+      userName: "FakeRoomOwner",
+      RoomID: joinedRoomInfo.RoomID,
+      RoomUserStatus: "OWNER",
+      profileColor: "orange",
+      joinedAt: "2024-08-02T21:34:22.328Z",
+    },
+    {
+      userID: fakeUserInfo.userID,
+      userName: fakeUserInfo.userName,
+      RoomID: joinedRoomInfo.RoomID,
+      RoomUserStatus: "MEMBER",
+      profileColor: fakeUserInfo.profileColor,
+      joinedAt: "2024-08-02T21:34:22.328Z",
+    },
+  ],
+  isMember: true,
+  isOwnerOrAdmin: false,
+  isOwner: false,
+  navJoinRequest: [],
+  profileColor: fakeUserInfo.profileColor,
+  username: fakeUserInfo.userName,
+  isProduction: false,
+};
+
+const fakeJoinRoomInfoPageOwner = {
+  roomInfo: ownedRoomInfo,
+  roomOwner: {
+    userID: fakeUserInfo.userID,
+    userName: fakeUserInfo.userName,
+    RoomID: joinedRoomInfo.RoomID,
+    RoomUserStatus: "OWNER",
+    profileColor: fakeUserInfo.profileColor,
+    joinedAt: "2024-08-02T21:34:22.328Z",
+  },
+  roomMembers: [
+    {
+      userID: "FakeMemberID",
+      userName: "FakeRoomMember",
+      RoomID: joinedRoomInfo.RoomID,
+      RoomUserStatus: "MEMBER",
+      profileColor: "orange",
+      joinedAt: "2024-08-02T21:34:22.328Z",
+    },
+    {
+      userID: fakeUserInfo.userID,
+      userName: fakeUserInfo.userName,
+      RoomID: joinedRoomInfo.RoomID,
+      RoomUserStatus: "OWNER",
+      profileColor: fakeUserInfo.profileColor,
+      joinedAt: "2024-08-02T21:34:22.328Z",
+    },
+  ],
+  isOwnerOrAdmin: true,
+  isOwner: true,
+  joinRequests: [
+    {
+      RoomID: ownedRoomInfo.RoomID,
+      fromUserID: "FakeJoinUserID",
+      fromUserName: "FakeJoinUserName",
+      roomName: ownedRoomInfo.roomName,
+      profileColor: "purple",
+      sentJoinRequestAt: "2024-08-02T21:34:22.328Z",
+    },
+  ],
+  navJoinRequest: [],
+  profileColor: fakeUserInfo.profileColor,
+  username: fakeUserInfo.userName,
+  isProduction: false,
+};
+
+export {
+  fakeDashboardData,
+  ownedRoomInfo,
+  joinedRoomInfo,
+  fakeJoinRoomInfoPageMember,
+  fakeJoinRoomInfoPageOwner,
+};
--- ejs-static/script.js
+++ ejs-static/script.js
@@ -1,14 +1,43 @@
 import express from "express";
 import path from "path";
+import {
+  fakeDashboardData,
+  ownedRoomInfo,
+  joinedRoomInfo,
+  fakeJoinRoomInfoPageOwner,
+  fakeJoinRoomInfoPageMember,
+} from "./fakeEjsPageData.js";
 const app = express();
 
 app.use(express.static("../dist/public"));
+app.set("view engine", "ejs");
 
 app.get("/about", (req, res) => {
   res.sendFile(path.resolve("..", "dist", "public", "index.html"));
 });
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
-  console.log("url: http://localhost:8000");
+app.get("/dashboard", (req, res) => {
+  res.render(
+    path.resolve("..", "serverless-aws-sam", "src", "views", "dashboard"),
+    fakeDashboardData
+  );
+});
+
+app.get(`/rooms/${ownedRoomInfo.RoomID}`, (req, res) => {
+  res.render(
+    path.resolve("..", "serverless-aws-sam", "src", "views", "roomInfo"),
+    fakeJoinRoomInfoPageOwner
+  );
+});
+
+app.get(`/rooms/${joinedRoomInfo.RoomID}`, (req, res) => {
+  res.render(
+    path.resolve("..", "serverless-aws-sam", "src", "views", "roomInfo"),
+    fakeJoinRoomInfoPageMember
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+  console.log("url: http://localhost:3000");
 });
