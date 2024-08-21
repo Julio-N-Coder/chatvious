e66f145ef8e6a1f2078c58b5f2a255dd5720d8d1
Julio <Julio.Navarro.Coder@gmail.com>
2024-08-21 11:56:07 -0700

    made delete account model and client code

--- ejs-static/public/ts/profilePage/profilePage.ts
+++ ejs-static/public/ts/profilePage/profilePage.ts
@@ -4,3 +4,83 @@ const deleteAccountConfirmButton = document.getElementById(
 const deleteAccountInput = document.getElementById(
   "DELETE_ACCOUNT_INPUT"
 ) as HTMLInputElement;
+const errorContainer = document.getElementById(
+  "error-container"
+) as HTMLDivElement;
+const errorText = document.getElementById("error-text") as HTMLSpanElement;
+const deleteRoomButtonLoader = document.getElementById(
+  "delete-room-button-loader"
+) as HTMLElement;
+
+Delete_Account_Model.showModal();
+
+function toggleConfirmDeleteButtonState() {
+  deleteAccountConfirmButton.classList.toggle("btn-disabled");
+  deleteRoomButtonLoader.classList.toggle("hidden");
+  deleteAccountConfirmButton.classList.toggle("px-1");
+}
+
+async function submitDeleteAccount() {
+  const deleteAccountInputValue = deleteAccountInput.value;
+
+  if (deleteAccountInputValue.toLocaleLowerCase() === "delete") {
+    toggleConfirmDeleteButtonState();
+    deleteAccountInput.value = "";
+    const deleteAccountUrl = `${process.env.DOMAIN_URL}/user/deleteAccount`;
+
+    let response: Response;
+    try {
+      response = await fetch(deleteAccountUrl, {
+        method: "POST",
+      });
+    } catch (error) {
+      errorContainer.classList.remove("invisible");
+      console.error("Error while trying to delete your account", error);
+      errorText.textContent = "Error deleting account";
+      toggleConfirmDeleteButtonState();
+      return;
+    }
+
+    if (!response.ok) {
+      errorContainer.classList.remove("invisible");
+
+      let jsonResponse;
+      try {
+        jsonResponse = await response.json();
+      } catch (error) {
+        console.error("Error while trying to delete your account", error);
+        errorText.textContent = "Error deleting account";
+        toggleConfirmDeleteButtonState();
+        return;
+      }
+      errorText.textContent = jsonResponse.error;
+      toggleConfirmDeleteButtonState();
+      return;
+    }
+
+    window.location.href = process.env.SUB_DOMAIN_URL as string;
+  }
+}
+
+deleteAccountConfirmButton.addEventListener("click", submitDeleteAccount);
+deleteAccountInput.addEventListener("keyup", (event) => {
+  const deleteAccountInputValue = deleteAccountInput.value;
+
+  if (event.key === "Enter" && deleteAccountInputValue.length > 0) {
+    submitDeleteAccount();
+  }
+});
+
+deleteAccountInput.addEventListener("input", () => {
+  const deleteAccountInputValue = deleteAccountInput.value;
+
+  if (deleteAccountInputValue.length > 0) {
+    deleteAccountConfirmButton.classList.remove("btn-disabled");
+  } else {
+    deleteAccountConfirmButton.classList.add("btn-disabled");
+  }
+});
+
+declare const Delete_Account_Model: {
+  showModal: () => void;
+};
