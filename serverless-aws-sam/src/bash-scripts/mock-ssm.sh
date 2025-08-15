a0c1c748ebc7e9f989b98538634db5a49a2518c3
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-08-14 18:26:59 -0700

    start mock-ssm bash script

---
+++ serverless-aws-sam/src/bash-scripts/mock-ssm.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+MOCK_SSM_SERVER_DIR="$(dirname "$SCRIPT_DIR")/aws-mock"
+
+check_keys() {
+	# keys are made from the ED25519 algorithm
+	private_key="${MOCK_SSM_SERVER_DIR}/keys/private_key.pem"
+	public_key="${MOCK_SSM_SERVER_DIR}/keys/public_key.pem"
+
+	if ! [ -f "$private_key" ]; then
+		echo "generating local private key"
+		openssl genpkey -algorithm ED25519 -out "$private_key"
+	fi
+
+	if ! [ -f "$public_key" ]; then
+		echo "generating local public key"
+		openssl pkey -in "$private_key" -pubout -out "$public_key"
+	fi
+}
+
+start_mock_ssm() {
+	check_keys
+
+	echo "Starting Mock SSM Parameter Store server..."
+
+	if ! command -v python3 &>/dev/null; then
+		echo "Error: python3 is required but not installed."
+		exit 1
+	fi
+
+	echo "Mock SSM server will run on http://localhost:8009"
+
+	python3 "${MOCK_SSM_SERVER_DIR}/mock_ssm_server.py"
+}
+
+start_mock_ssm
