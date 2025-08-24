51ee2ab9ba0e1805eceefd6386c51ff83ccb0dcd
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-08-24 16:05:44 -0700

    moved npm-test in bash test directory

--- serverless-aws-sam/src/bash-scripts/tests/npm-test.sh
+++ serverless-aws-sam/src/bash-scripts/tests/npm-test.sh
@@ -3,18 +3,25 @@
 # This script is a helper script for npm test to start up a dynamodb container
 # for them to use and run the correct test with the specified arguments
 
-set -e
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-SRC_DIR=$(dirname "${SCRIPT_DIR}")
+set -euo pipefail
+BASH_SCRIPTS_DIR="$(dirname "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)")"
+SRC_DIR=$(dirname "${BASH_SCRIPTS_DIR}")
 DYNAMODB_CONTAINER_NAME="chatvious-dynamodb-3821"
 
 # run dynamodb starting script in the background and save pid
-"${SCRIPT_DIR}/dynamodb-start.sh" "$DYNAMODB_CONTAINER_NAME"
+"${BASH_SCRIPTS_DIR}/dynamodb-start.sh" "$DYNAMODB_CONTAINER_NAME"
 
-source "${SCRIPT_DIR}/utils/db-helpers.sh"
+source "${BASH_SCRIPTS_DIR}/utils/db-helpers.sh"
 wait_for_dynamodb
 create_db_table "chatvious-test"
 echo "DynamoDB is ready."
+
+cleanup() {
+	echo "Stopping and removing container"
+	docker stop "${DYNAMODB_CONTAINER_NAME}"
+	docker rm "${DYNAMODB_CONTAINER_NAME}"
+}
+trap cleanup EXIT
 
 # run all test if no arguments
 if [ $# -eq 0 ]; then
@@ -34,7 +41,3 @@ fi
 
 cd ${SRC_DIR}
 NODE_OPTIONS=--experimental-vm-modules npx jest $inBand "${SRC_DIR}/${path}"
-
-echo "Stopping and removing container"
-docker stop "${DYNAMODB_CONTAINER_NAME}"
-docker rm "${DYNAMODB_CONTAINER_NAME}"
