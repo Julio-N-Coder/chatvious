ec7f320cdf42eb4e1672734b18cff60385952d59
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-09-08 16:32:19 -0700

    test: kickMember test mock dynamodb doc client

--- serverless-aws-sam/src/roomRoutes/kickMember/tests/kickMember.test.ts
+++ serverless-aws-sam/src/roomRoutes/kickMember/tests/kickMember.test.ts
@@ -1,83 +1,38 @@
 import { handler } from "../kickMember.js";
 import restAPIEventBase from "../../../../events/restAPIEvent.json";
+import { describe, test, expect, beforeAll, afterEach } from "@jest/globals";
+import { RoomMemberDB } from "../../../types/types.js";
+import { mockClient } from "aws-sdk-client-mock";
 import {
-  describe,
-  test,
-  expect,
-  beforeAll,
-  afterAll,
-  afterEach,
-} from "@jest/globals";
-import { userManager } from "../../../models/users.js";
-import { roomManager, roomUsersManager } from "../../../models/rooms.js";
-import { UserInfo, RoomInfoType } from "../../../types/types.js";
+  DynamoDBDocumentClient,
+  GetCommand,
+  UpdateCommand,
+  DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
 import {
-  newTestUser,
-  checkRoomsOnUser,
-  clearDynamoDB,
-} from "../../../lib/libtest/handyTestUtils.js";
+  $metadata,
+  userInfoDB,
+  roomInfoDB,
+  roomMemberDB,
+} from "../../../lib/libtest/testData.js";
+
+const ddbMock = mockClient(DynamoDBDocumentClient);
 
 let restAPIEvent: typeof restAPIEventBase = JSON.parse(
   JSON.stringify(restAPIEventBase)
 );
 let restAPIEventCopy: typeof restAPIEventBase;
 
-const userID = restAPIEvent.requestContext.authorizer.sub;
-const userName = restAPIEvent.requestContext.authorizer.username;
-const email = restAPIEvent.requestContext.authorizer.email;
-let newUser: UserInfo;
+let RoomID = roomInfoDB.RoomID;
+const roomName = roomInfoDB.roomName;
 
-const roomName = "kickMemberTestRoom";
-let roomInfo: RoomInfoType;
-let RoomID: string;
-
-let userBeingKicked: UserInfo;
-let userBeingKickedID: string;
-let userBeingKickedName: string;
+let userBeingKicked = structuredClone(userInfoDB);
+let userBeingKickedID = "z7574571-6cd1-4fbb-ba4f-43c39573729a";
+let userBeingKickedName = "userPromotedDemotedName";
+userBeingKicked.userID = userBeingKickedID;
+userBeingKicked.userName = userBeingKickedName;
 
 beforeAll(async () => {
-  newUser = await newTestUser(userID, userName);
-
-  // make a user for the person being kicked
-  const createuserBeingKickedResponse = await userManager.createUser();
-  if ("error" in createuserBeingKickedResponse) {
-    throw new Error(
-      `Failed to create user. Error: ${createuserBeingKickedResponse.error}`
-    );
-  }
-  userBeingKicked = createuserBeingKickedResponse.newUser;
-  userBeingKickedID = userBeingKicked.userID;
-  userBeingKickedName = userBeingKicked.userName;
-
-  // make a room for the user to be kicked from
-  const createRoomResponse = await roomManager.makeRoom(
-    userID,
-    userName,
-    roomName,
-    newUser.profileColor
-  );
-  if ("error" in createRoomResponse) {
-    throw new Error(
-      `Failed to create room. Error: ${createRoomResponse.error}`
-    );
-  }
-  roomInfo = createRoomResponse.roomInfo;
-  RoomID = roomInfo.RoomID;
-
-  // insert the user being kicked into the room
-  const addRoomMemberResponse = await roomUsersManager.addRoomMember(
-    RoomID,
-    userBeingKickedID,
-    roomName,
-    userBeingKickedName,
-    userBeingKicked.profileColor
-  );
-  if ("error" in addRoomMemberResponse) {
-    throw new Error(
-      `Failed to add room member. Error: ${addRoomMemberResponse.error}`
-    );
-  }
-
   restAPIEvent.body = JSON.stringify({
     userID: userBeingKickedID,
     RoomID,
@@ -89,51 +44,59 @@ beforeAll(async () => {
 });
 
 afterEach(async () => {
+  ddbMock.reset();
   restAPIEvent = JSON.parse(JSON.stringify(restAPIEventCopy));
-});
-
-afterAll(async () => {
-  await clearDynamoDB();
 });
 
 // make sure to test room user status as well
 describe("Test if kickMember route kicks the user from the chat room", () => {
   test("kickMember route returns successfull response and removes member", async () => {
+    ddbMock.on(GetCommand).callsFake((input) => {
+      if (input.Key.SortKey === roomMemberDB.SortKey) {
+        return Promise.resolve({
+          $metadata,
+          Item: roomMemberDB,
+        });
+
+        // fetchRoomsOnUser
+      } else if (input.Key.SortKey === "PROFILE") {
+        return Promise.resolve({
+          $metadata,
+          Item: { joinedRooms: [{ RoomID, roomName }], ownedRooms: [] },
+        });
+      }
+
+      return Promise.resolve({
+        $metadata,
+        Item: {
+          ...roomMemberDB,
+          userID: userBeingKickedID,
+          userName: userBeingKickedName,
+          RoomUserStatus: "MEMBER",
+        } as RoomMemberDB,
+      });
+    });
+    ddbMock.on(DeleteCommand).resolves({
+      $metadata,
+      Attributes: {},
+    });
+    ddbMock.on(UpdateCommand).resolves({
+      $metadata,
+      Attributes: {},
+    });
+
     const response = await handler(restAPIEvent);
     expect(response).toHaveProperty("statusCode", 200);
 
     const body = JSON.parse(response.body);
     expect(body.message).toBe("User Successfully Kicked");
 
-    // check if member has been kicked
-    const fetchRoomMemberResponse = await roomUsersManager.fetchRoomMember(
-      RoomID,
-      userBeingKickedID
-    );
-    if (
-      "error" in fetchRoomMemberResponse &&
-      fetchRoomMemberResponse.error !== "Bad Request"
-    ) {
-      throw new Error(
-        `Failed to fetch roomMember in test. Error: ${fetchRoomMemberResponse.error}`
-      );
-    }
+    // check if member has been attempted to be kicked
+    expect(ddbMock.commandCalls(DeleteCommand)).toHaveLength(1);
 
-    expect(fetchRoomMemberResponse).toHaveProperty("statusCode", 400);
-    expect(fetchRoomMemberResponse).toHaveProperty("error", "Bad Request");
-
-    await checkRoomsOnUser(userBeingKickedID, RoomID, roomName, "Removed");
-
-    // check if memberCount was decreased
-    const fetchRoomResponse = await roomManager.fetchRoom(RoomID);
-    if ("error" in fetchRoomResponse) {
-      throw new Error(
-        `Failed to fetch room. Error: ${fetchRoomResponse.error}`
-      );
-    }
-
-    const memberCount = fetchRoomResponse.roomInfo.roomMemberCount;
-    expect(memberCount).toBe(1);
+    // check if room on user is attempted to be removed
+    // check if memberCount is attempted to be decreased
+    expect(ddbMock.commandCalls(UpdateCommand)).toHaveLength(2);
   });
 
   test("Incorrect Content-Type header should return the correct Error", async () => {
