17e96f1963e2f852054a6a0ea489f662b5dabfe7
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-09-09 13:24:28 -0700

    test: WSDisconnect mock dynamodb doc client

--- serverless-aws-sam/src/websocket-endpoints/disconnect/tests/disconnect.test.ts
+++ serverless-aws-sam/src/websocket-endpoints/disconnect/tests/disconnect.test.ts
@@ -1,106 +1,37 @@
 import { handler } from "../disconnect.js";
 import restAPIEventBase from "../../../../events/websocketApiDisconnectEvent.json";
+import { describe, test, expect, beforeAll, afterEach } from "@jest/globals";
+import { APIGatewayWebSocketDisconnectEvent } from "../../../types/types.js";
+import { mockClient } from "aws-sdk-client-mock";
+import { DynamoDBDocumentClient, DeleteCommand } from "@aws-sdk/lib-dynamodb";
 import {
-  initialConectDBWSManager,
-  roomConnectionsWSManager,
-} from "../../../models/web-socket-messages.js";
-import { userManager } from "../../../models/users.js";
-import { roomManager } from "../../../models/rooms.js";
-import { describe, test, expect, beforeAll, afterAll } from "@jest/globals";
-import {
-  APIGatewayWebSocketDisconnectEvent,
-  UserInfo,
-  RoomInfoType,
-} from "../../../types/types.js";
-import { clearDynamoDB } from "../../../lib/libtest/handyTestUtils.js";
+  $metadata,
+  initialConnectionDB,
+} from "../../../lib/libtest/testData.js";
+
+const ddbMock = mockClient(DynamoDBDocumentClient);
 
 const restAPIEvent = restAPIEventBase as APIGatewayWebSocketDisconnectEvent;
 const connectionId = restAPIEvent.requestContext.connectionId;
 
-let userInfo: UserInfo;
-let userID: string;
-let userName: string;
-
-let roomInfo: RoomInfoType;
-const roomName = "disconnectTestRoom";
-let RoomID: string;
-
 beforeAll(async () => {
-  // make a test user
-  const makeUserResponse = await userManager.createUser();
-  if ("error" in makeUserResponse) {
-    throw new Error(
-      `Error while making a user. Error: ${makeUserResponse.error}`
-    );
-  }
-  userInfo = makeUserResponse.newUser;
-  userID = userInfo.userID;
-  userName = userInfo.userName;
-
-  // make a room for the user to be a part of
-  const makeRoomResponse = await roomManager.makeRoom(
-    userID,
-    userName,
-    roomName,
-    userInfo.profileColor
-  );
-  if ("error" in makeRoomResponse) {
-    throw new Error(
-      `Error while making a room. Error: ${makeRoomResponse.error}`
-    );
-  }
-  roomInfo = makeRoomResponse.roomInfo;
-  RoomID = roomInfo.RoomID;
-
-  // store initial Connection Information
-  const storeInitialConnectionResponse =
-    await initialConectDBWSManager.storeInitialConnection(
-      connectionId,
-      userID,
-      RoomID
-    );
-  if ("error" in storeInitialConnectionResponse) {
-    throw new Error(
-      `Error while storing initial connection. Error: ${storeInitialConnectionResponse.error}`
-    );
-  }
-
-  // store room connection information
-  const storeRoomConnectionResponse =
-    await roomConnectionsWSManager.storeRoomConnection(
-      connectionId,
-      userID,
-      RoomID,
-      userName,
-      "OWNER",
-      userInfo.profileColor
-    );
-  if ("error" in storeRoomConnectionResponse) {
-    throw new Error(
-      `Error while storing room connection. Error: ${storeRoomConnectionResponse.error}`
-    );
-  }
-
   restAPIEvent.requestContext.connectionId = connectionId;
 });
 
-afterAll(async () => {
-  await clearDynamoDB();
+afterEach(async () => {
+  ddbMock.reset();
 });
 
 describe("A test for the disconnect route on the api gateway websocket", () => {
   test("Should return a successfull response and correctly remove connection information", async () => {
+    ddbMock.on(DeleteCommand).resolves({
+      $metadata,
+      Attributes: initialConnectionDB,
+    });
+
     const response = await handler(restAPIEvent);
     expect(response).toHaveProperty("statusCode", 200);
 
-    const initialConnectionCheck =
-      await initialConectDBWSManager.fetchInitialConnection(connectionId);
-    expect(initialConnectionCheck).toHaveProperty("error", "No data found");
-    expect(initialConnectionCheck).toHaveProperty("statusCode", 404);
-
-    const roomConnectionCheck =
-      await roomConnectionsWSManager.fetchRoomConnection(RoomID, connectionId);
-    expect(roomConnectionCheck).toHaveProperty("error", "No data found");
-    expect(roomConnectionCheck).toHaveProperty("statusCode", 404);
+    expect(ddbMock.commandCalls(DeleteCommand)).toHaveLength(2);
   });
 });
