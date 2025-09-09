de16620964a59bab449584ef8a04baef8bfa22c9
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-09-09 14:32:47 -0700

    chore: remove npm-test dynamodb startup and leftover cognito data

--- serverless-aws-sam/src/bash-scripts/tests/npm-test.sh
+++ serverless-aws-sam/src/bash-scripts/tests/npm-test.sh
@@ -1,28 +1,9 @@
 #!/usr/bin/env bash
-
-# This script is a helper script for npm test to start up a dynamodb container
-# for them to use and run the correct test with the specified arguments
-
 set -euo pipefail
+
 BASH_SCRIPTS_DIR="$(dirname "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)")"
 SRC_DIR=$(dirname "${BASH_SCRIPTS_DIR}")
 DYNAMODB_CONTAINER_NAME="chatvious-dynamodb-3821"
-
-# run dynamodb starting script in the background and save pid
-"${BASH_SCRIPTS_DIR}/dynamodb-start.sh" "$DYNAMODB_CONTAINER_NAME"
-
-source "${BASH_SCRIPTS_DIR}/utils/db-helpers.sh"
-wait_for_dynamodb
-create_db_table "chatvious-test"
-insert_limits_item "chatvious-test"
-echo "DynamoDB is ready."
-
-cleanup() {
-	echo "Stopping and removing container"
-	docker stop "${DYNAMODB_CONTAINER_NAME}"
-	docker rm "${DYNAMODB_CONTAINER_NAME}"
-}
-trap cleanup EXIT
 
 # run all test if no arguments
 if [ $# -eq 0 ]; then
