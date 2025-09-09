96379326ba59041d01e573faed0f878867f6fdea
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-09-09 14:25:16 -0700

    test: sendMessageWS mock dynamodb doc client and api gateway management client

--- serverless-aws-sam/src/websocket-endpoints/sendMessage/tests/sendMessage.test.ts
+++ serverless-aws-sam/src/websocket-endpoints/sendMessage/tests/sendMessage.test.ts
@@ -1,82 +1,47 @@
 import { handler } from "../sendMessage.js";
 import restAPIEventBase from "../../../../events/websocketApiCustomEvent.json";
-import { roomConnectionsWSManager } from "../../../models/web-socket-messages.js";
-import { userManager } from "../../../models/users.js";
-import { roomManager } from "../../../models/rooms.js";
-import { messagesManagerDB } from "../../../models/messagesDB.js";
 import {
-  jest,
   describe,
   test,
   expect,
   beforeAll,
-  afterAll,
   afterEach,
+  beforeEach,
 } from "@jest/globals";
 import { APIGatewayProxyWebsocketEventV2 } from "aws-lambda";
-import { UserInfo, RoomInfoType } from "../../../types/types.js";
-import { ApiGatewayManagementApiClient } from "@aws-sdk/client-apigatewaymanagementapi";
-import { clearDynamoDB } from "../../../lib/libtest/handyTestUtils.js";
+import { RoomConnectionDB } from "../../../types/types.js";
+import {
+  ApiGatewayManagementApiClient,
+  PostToConnectionCommand,
+} from "@aws-sdk/client-apigatewaymanagementapi";
+import { mockClient } from "aws-sdk-client-mock";
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  QueryCommand,
+  UpdateCommand,
+  PutCommand,
+} from "@aws-sdk/lib-dynamodb";
+import {
+  $metadata,
+  roomInfoDB,
+  roomConnectionDB,
+} from "../../../lib/libtest/testData.js";
+
+const ddbMock = mockClient(DynamoDBDocumentClient);
+const apiGatewayMock = mockClient(ApiGatewayManagementApiClient);
 
 let restAPIEvent = restAPIEventBase as APIGatewayProxyWebsocketEventV2;
 let restAPIEventCopy: APIGatewayProxyWebsocketEventV2;
 const connectionId = restAPIEvent.requestContext.connectionId;
 
-let userInfo: UserInfo;
-let userID: string;
-let userName: string;
-
-let roomInfo: RoomInfoType;
-const roomName = "SendMessageTestRoom";
-let RoomID: string;
+let RoomID = roomInfoDB.RoomID;
 
 let messageId: string;
 let messageDate: string;
 const message = "This is a test message";
 
 beforeAll(async () => {
-  // make a test user
-  const makeUserResponse = await userManager.createUser();
-  if ("error" in makeUserResponse) {
-    throw new Error(
-      `Error while making a user. Error: ${makeUserResponse.error}`
-    );
-  }
-  userInfo = makeUserResponse.newUser;
-  userID = userInfo.userID;
-  userName = userInfo.userName;
-
-  // make a room to check whether user is part of room
-  const makeRoomResponse = await roomManager.makeRoom(
-    userID,
-    userName,
-    roomName,
-    userInfo.profileColor
-  );
-  if ("error" in makeRoomResponse) {
-    throw new Error(
-      `Error while making a room. Error: ${makeRoomResponse.error}`
-    );
-  }
-  roomInfo = makeRoomResponse.roomInfo;
-  RoomID = roomInfo.RoomID;
-
-  // store join room Connection Information
-  const roomConnectionResponse =
-    await roomConnectionsWSManager.storeRoomConnection(
-      connectionId,
-      userID,
-      RoomID,
-      userName,
-      "OWNER",
-      userInfo.profileColor
-    );
-  if ("error" in roomConnectionResponse) {
-    throw new Error(
-      `Error while storing room connection. Error: ${roomConnectionResponse.error}`
-    );
-  }
-
   restAPIEvent.body = JSON.stringify({
     action: "sendmessage",
     message,
@@ -88,24 +53,35 @@ beforeAll(async () => {
   restAPIEventCopy = JSON.parse(JSON.stringify(restAPIEvent));
 });
 
-afterAll(async () => {
-  await clearDynamoDB();
+beforeEach(async () => {
+  ddbMock.on(GetCommand).resolves({
+    $metadata,
+    Item: roomConnectionDB,
+  });
+  ddbMock.on(UpdateCommand).resolves({
+    $metadata,
+  });
+  ddbMock.on(QueryCommand).resolves({
+    $metadata,
+    Items: [roomConnectionDB] as RoomConnectionDB[],
+  });
+  ddbMock.on(PutCommand).resolves({
+    $metadata,
+    Attributes: {},
+  });
+  apiGatewayMock
+    .on(PostToConnectionCommand)
+    .resolves({ $metadata: { httpStatusCode: 200 } });
 });
 
 afterEach(async () => {
-  jest.clearAllMocks();
+  ddbMock.reset();
+  apiGatewayMock.reset();
   restAPIEvent = JSON.parse(JSON.stringify(restAPIEventCopy));
 });
 
 describe("A test for the custom joinRoom route on the api gateway websocket", () => {
   test("Should return a successfull response and correctly store message information correclty", async () => {
-    // @ts-ignore
-    ApiGatewayManagementApiClient.prototype.send = jest
-      .fn()
-      .mockImplementationOnce(() => ({
-        promise: () => Promise.resolve(),
-      }));
-
     const response = await handler(restAPIEvent);
     expect(response).toHaveProperty("statusCode", 200);
     expect(response).toHaveProperty("body", "Message sent successfully");
@@ -119,24 +95,8 @@ describe("A test for the custom joinRoom route on the api gateway websocket", ()
     expect(response).toHaveProperty("messageId", messageId);
     expect(response).toHaveProperty("messageDate", messageDate);
 
-    // check whether the message was correctly stored
-    const getMessageResponse = await messagesManagerDB.fetchMessage(
-      RoomID,
-      messageDate,
-      messageId
-    );
-    if ("error" in getMessageResponse) {
-      throw new Error(
-        `Error while fetching message in test. Error: ${getMessageResponse.error}`
-      );
-    }
-
-    const messageData = getMessageResponse.data;
-    expect(messageData).toHaveProperty("RoomID", RoomID);
-    expect(messageData).toHaveProperty("message", message);
-    expect(messageData).toHaveProperty("messageId", messageId);
-    expect(messageData).toHaveProperty("sentAt", messageDate);
-    expect(messageData).toHaveProperty("userID", userID);
+    // check whether the message is attempted to be stored
+    expect(ddbMock.commandCalls(PutCommand)).toHaveLength(1);
   });
 
   test("Should return correct error when message is missing from body", async () => {
