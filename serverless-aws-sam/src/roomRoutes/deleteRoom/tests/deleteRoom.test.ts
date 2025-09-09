e165caf8c71ee34525072c27a6575201ba6c69d6
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-09-08 22:44:12 -0700

    test: deleteRoom test mock dynamodb doc client

--- serverless-aws-sam/src/roomRoutes/deleteRoom/tests/deleteRoom.test.ts
+++ serverless-aws-sam/src/roomRoutes/deleteRoom/tests/deleteRoom.test.ts
@@ -5,54 +5,38 @@ import {
   test,
   expect,
   beforeAll,
-  afterAll,
   afterEach,
+  beforeEach,
 } from "@jest/globals";
+import { BaseKeys } from "../../../types/types.js";
+import { mockClient } from "aws-sdk-client-mock";
 import {
-  roomManager,
-  roomUsersManager,
-  joinRequestManager,
-} from "../../../models/rooms.js";
-import { userManager } from "../../../models/users.js";
-import { messagesManagerDB } from "../../../models/messagesDB.js";
-import { UserInfo, RoomInfoType } from "../../../types/types.js";
+  DynamoDBDocumentClient,
+  GetCommand,
+  DeleteCommand,
+  QueryCommand,
+  UpdateCommand,
+  BatchWriteCommand,
+} from "@aws-sdk/lib-dynamodb";
 import {
-  newTestUser,
-  clearDynamoDB,
-  checkRoomsOnUser,
-} from "../../../lib/libtest/handyTestUtils.js";
+  $metadata,
+  userInfoDB,
+  roomInfoDB,
+  roomMemberDB,
+  messageDB,
+  joinRequestDB,
+} from "../../../lib/libtest/testData.js";
+
+const ddbMock = mockClient(DynamoDBDocumentClient);
 
 let restAPIEvent: typeof restAPIEventBase = JSON.parse(
   JSON.stringify(restAPIEventBase)
 );
 let restAPIEventCopy: typeof restAPIEventBase;
 
-const userID = restAPIEvent.requestContext.authorizer.sub;
-const userName = restAPIEvent.requestContext.authorizer.username;
-let newUser: UserInfo;
-
-const roomName = "deleteRoomTestRoom";
-let roomInfo: RoomInfoType;
-let RoomID: string;
+let RoomID = roomInfoDB.RoomID;
 
 beforeAll(async () => {
-  newUser = await newTestUser(userID, userName);
-
-  // make a room for the user to delete
-  const createRoomResponse = await roomManager.makeRoom(
-    userID,
-    userName,
-    roomName,
-    newUser.profileColor
-  );
-  if ("error" in createRoomResponse) {
-    throw new Error(
-      `Failed to create room. Error: ${createRoomResponse.error}`
-    );
-  }
-  roomInfo = createRoomResponse.roomInfo;
-  RoomID = roomInfo.RoomID;
-
   restAPIEvent.body = JSON.stringify({
     RoomID,
   });
@@ -62,139 +46,120 @@ beforeAll(async () => {
   restAPIEventCopy = JSON.parse(JSON.stringify(restAPIEvent));
 });
 
-let remakeRoom = false;
+beforeEach(async () => {
+  ddbMock.on(GetCommand).callsFake((input) => {
+    if (input.Key.PartitionKey === roomMemberDB.PartitionKey) {
+      return Promise.resolve({
+        $metadata,
+        Item: roomMemberDB,
+      });
+    }
+
+    return Promise.resolve({
+      $metadata,
+      Item: {
+        joinedRooms: userInfoDB.joinedRooms,
+        ownedRooms: userInfoDB.ownedRooms,
+      },
+    });
+  });
+  ddbMock.on(DeleteCommand).resolves({
+    $metadata,
+    Attributes: {},
+  });
+  ddbMock.on(UpdateCommand).resolves({
+    $metadata,
+    Attributes: {},
+  });
+});
 
 afterEach(async () => {
-  if (remakeRoom) {
-    const createRoomResponse = await roomManager.makeRoom(
-      userID,
-      userName,
-      roomName,
-      newUser.profileColor
-    );
-    if ("error" in createRoomResponse) {
-      throw new Error(
-        `Failed to create room. Error: ${createRoomResponse.error}`
-      );
-    }
-    roomInfo = createRoomResponse.roomInfo;
-    RoomID = roomInfo.RoomID;
-  }
+  ddbMock.reset();
+
   restAPIEvent = JSON.parse(JSON.stringify(restAPIEventCopy));
   restAPIEvent.body = JSON.stringify({
     RoomID,
   });
 });
 
-afterAll(async () => {
-  await clearDynamoDB();
-});
-
 describe("A Test for The deleteRoom Route", () => {
   test("Should return a successfull response and deletes the room", async () => {
+    ddbMock.on(QueryCommand).callsFake((input) => {
+      if (input.ExpressionAttributeValues[":sortDate"] === "JOIN_REQUESTS#") {
+        return Promise.resolve({
+          $metadata,
+          Count: 0,
+          Items: [],
+          ScannedCount: 0,
+        });
+      } else if (
+        input.ExpressionAttributeValues[":RoomMembersPrefix"] === "MEMBERS#"
+      ) {
+        return Promise.resolve({
+          $metadata,
+          Count: 1,
+          Items: [roomMemberDB],
+          ScannedCount: 1,
+        });
+      }
+      return Promise.resolve({
+        $metadata,
+        Count: 0,
+        Items: [],
+        ScannedCount: 0,
+      });
+    });
+
     const response = await handler(restAPIEvent);
     expect(response.statusCode).toBe(200);
     expect(JSON.parse(response.body).message).toBe("Room Deleted successfully");
 
-    // check if room was deleted
-    const fetchRoomResponse = await roomManager.fetchRoom(RoomID);
-    if (
-      "error" in fetchRoomResponse &&
-      fetchRoomResponse.error !== "Bad Request"
-    ) {
-      throw new Error(
-        `Failed to fetch room in test. Error: ${fetchRoomResponse.error}`
-      );
-    }
-
-    expect(fetchRoomResponse).toHaveProperty("statusCode", 400);
-    expect(fetchRoomResponse).toHaveProperty("error", "Bad Request");
-    await checkRoomsOnUser(userID, RoomID, roomName, "Removed");
-    remakeRoom = true;
+    // check if room was attempted to be deleted
+    // Check if room member was attempted to be deleted
+    expect(ddbMock.commandCalls(DeleteCommand)).toHaveLength(2);
   });
 
   test("Should correctly delete room with multiple resouces", async () => {
-    const fakeUserData = {
-      userName: "FakeTestName",
-      userID: "fakeUserID",
-      email: "fakeTestEmail@test.com",
-      profileColor: "green",
-      fakeStatus: "MEMBER",
-      fakeMadeDate: "fakeMadeDate",
-    };
+    ddbMock.on(QueryCommand).callsFake((input) => {
+      if (input.ExpressionAttributeValues[":sortDate"] === "JOIN_REQUESTS#") {
+        return Promise.resolve({
+          $metadata,
+          Count: 1,
+          Items: [
+            {
+              PartitionKey: joinRequestDB.PartitionKey,
+              SortKey: joinRequestDB.SortKey,
+            },
+          ] as BaseKeys[],
+          ScannedCount: 1,
+        });
+      } else if (
+        input.ExpressionAttributeValues[":RoomMembersPrefix"] === "MEMBERS#"
+      ) {
+        return Promise.resolve({
+          $metadata,
+          Count: 2,
+          Items: [roomMemberDB, roomMemberDB],
+          ScannedCount: 2,
+        });
+      }
 
-    const fakeMessageData = {
-      message: "fake message",
-      messageId: "fakeMessageID",
-      messageDate: "fakeTimestamp",
-    };
-
-    // make a user to use
-    const addUserResponse = await userManager.createUser(
-      fakeUserData.userID,
-      fakeUserData.userName,
-      fakeUserData.email,
-      fakeUserData.profileColor
-    );
-    if ("error" in addUserResponse) {
-      throw new Error(
-        `Failed to add user testing data. Error: ${addUserResponse.error}`
-      );
-    }
-
-    // add join request
-    const joinRequestResponse = await joinRequestManager.sendJoinRequest(
-      fakeUserData.userName,
-      fakeUserData.userID,
-      roomName,
-      RoomID,
-      fakeUserData.profileColor
-    );
-    if ("error" in joinRequestResponse) {
-      throw new Error(
-        `Failed to add join request testing data. Error: ${joinRequestResponse.error}`
-      );
-    }
-
-    // add roomMembers
-    const addRoomMemberResponse = await roomUsersManager.addRoomMember(
-      RoomID,
-      fakeUserData.userID,
-      roomName,
-      fakeUserData.userName,
-      fakeUserData.profileColor,
-      fakeUserData.fakeMadeDate
-    );
-    if ("error" in addRoomMemberResponse) {
-      throw new Error(
-        `Failed to add room member testing data. Error: ${addRoomMemberResponse.error}`
-      );
-    }
-
-    // add messages
-    const addMessageResponse = await messagesManagerDB.storeMessage(
-      fakeUserData.userID,
-      fakeUserData.userName,
-      RoomID,
-      fakeUserData.fakeStatus as "MEMBER",
-      fakeUserData.profileColor,
-      fakeMessageData.message,
-      fakeMessageData.messageId,
-      fakeMessageData.messageDate
-    );
-    if ("error" in addMessageResponse) {
-      throw new Error(
-        `Failed to add message testing data. Error: ${addMessageResponse.error}`
-      );
-    }
+      return Promise.resolve({
+        $metadata,
+        Count: 1,
+        Items: [messageDB],
+        ScannedCount: 1,
+      });
+    });
+    ddbMock.on(BatchWriteCommand).resolves({
+      $metadata,
+      UnprocessedItems: {},
+    });
 
     const response = await handler(restAPIEvent);
     const body = JSON.parse(response.body);
     expect(body).toHaveProperty("message", "Room Deleted successfully");
     expect(response.statusCode).toBe(200);
-    await checkRoomsOnUser(userID, RoomID, roomName, "Removed");
-    await checkRoomsOnUser(fakeUserData.userID, RoomID, roomName, "Removed");
-    remakeRoom = false;
   });
 
   test("Incorrect Content-Type header should return the correct Error", async () => {
