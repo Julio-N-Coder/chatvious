d9a94f08fb704af3260dff9247c583ec6578c767
Julio-N-Coder <julio.navarro.coder@gmail.com>
2026-01-01 18:07:22 -0800

    feat: wait for main page to be up in dev.sh

--- dev.sh
+++ dev.sh
@@ -30,6 +30,17 @@ echo "Serving static resources"
 # static command is also shutdown when "ctrl+c" is pressed
 npm run static &
 
+LOOP_AMOUNT=0
+MAX_LOOP=10
+until curl -fs https://sub.main.localhost:8040 >/dev/null; do
+	if [ $LOOP_AMOUNT -ge $MAX_LOOP ]; then
+		echo "Static Page Timed Out" >&2
+		kill $!
+	fi
+	LOOP_AMOUNT=$(($LOOP_AMOUNT + 1))
+	sleep 0.2
+done
+
 xdg-open https://sub.main.localhost:8040
 
 cd "${PROJECT_DIR}/serverless-aws-sam"
